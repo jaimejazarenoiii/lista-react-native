8e7deb9d022a0cbb4e5759e5e4b5be933b2d92f9
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2021-09-18 14:29:05 +0800

    Add navigation stack in app

--- App.tsx
+++ App.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
-import { SafeAreaView, StatusBar, } from 'react-native'
+import { StatusBar, } from 'react-native'
 import { Provider } from 'react-redux'
 import { ShopList } from './src/containers'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { store } from './src/redux'
+import { RootStackParamList } from './src/enums'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <StatusBar barStyle="dark-content" hidden />
-      <SafeAreaView>
-        <ShopList/>
-      </SafeAreaView>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="ShopList" component={ ShopList }
+              options={({ navigation, route }) => ({ headerLargeTitle: true,
+                headerTitle: "Shopping Sessions"
+                })} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </Provider>
   )
 }
